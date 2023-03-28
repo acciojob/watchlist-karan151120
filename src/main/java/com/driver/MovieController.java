94b1a8730a2f8949dc7f5ac3f65388d4d6c30515
karan151120 <karantulsani28@gmail.com>
2023-03-28 14:13:23 +0530

    28.1 mar

--- src/main/java/com/driver/MovieController.java
+++ src/main/java/com/driver/MovieController.java
@@ -51,7 +51,7 @@ public class MovieController {
     }
 
     @GetMapping("/get-all-movies")
-    public ResponseEntity<List<String>> getAllMovies(){
+    public ResponseEntity<List<String>> findAllMovies(){
         List<String> movies = null; // Assign list of student by calling service layer method
         movies = movieService.getAllMovies();
         return new ResponseEntity<>(movies, HttpStatus.CREATED);
